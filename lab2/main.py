d1795bba7da0c445cdbe0d734df776da37027e96
chekulaev17 <dimachekulaev17@gmail.com>
2024-10-30 14:31:52 +0400

    Update

--- lab2/main.py
+++ lab2/main.py
@@ -3,18 +3,25 @@ from crawler import fetch_images
 from iterator import ImageAnnotationIterator
 from parser import parse_cli_args
 
+
 def main():
-    args = parse_cli_args()
-    fetch_images(args.search_keyword, args.image_count, args.output_folder)
-    generate_annotation(args.output_folder, args.csv_annotation)
-    print("Iterating over images:")
-    iterator = ImageAnnotationIterator(args.csv_annotation if args.from_csv else args.output_folder, from_csv=args.from_csv)
-    for annotation in iterator:
-        if args.from_csv:
-            print(f"Absolute Path: {annotation[0]}, Relative Path: {annotation[1]}")
-        else:
-            print(f"Image Path: {annotation}")
+    try:
+        args = parse_cli_args()
+        fetch_images(args.search_keyword, args.image_count, args.output_folder)
+        generate_annotation(args.output_folder, args.csv_annotation)
+        print("Iterating over images:")
+        iterator = ImageAnnotationIterator(
+            args.csv_annotation if args.from_csv else args.output_folder, from_csv=args.from_csv
+        )
+        for annotation in iterator:
+            if args.from_csv:
+                print(f"Absolute Path: {annotation[0]}, Relative Path: {annotation[1]}")
+            else:
+                print(f"Image Path: {annotation}")
+    except Exception as e:
+        print(f"Error in the main function: {e}")
 
 
 if __name__ == "__main__":
     main()
+
